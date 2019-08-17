96e769c174a7e39755bf78c564cdb4bd1e27239c
Nick Gottschlich <nicholas.gottschlich@procore.com>
2019-08-16 21:22:36 -0500

    more work on app

--- src/Containers/MainScreen.js
+++ src/Containers/MainScreen.js
@@ -1,19 +1,39 @@
+// NPM
 import React from 'react';
+
+// Local
 import fbLogo from '../assets/fbLogo.png';
+
+// CSS
 import './MainScreen.css';
 
 export default class MainScreen extends React.Component {
   render() {
     return (
       <div className="App">
-        <button className="facebookButton">
-          <img 
-            className="facebookLogo"
-            src={fbLogo}
-            alt="fucc the zucc" 
-          />
-          Flush Facebook
-        </button>
+        <div className="linkContainer">
+          <a 
+            href="https://www.facebook.com/help/delete_account" 
+            target="_blank" 
+            className="button">
+            <img 
+              className="facebookLogo"
+              src={fbLogo}
+              alt="fucc the zucc" 
+            />
+            Flush Facebook
+          </a>
+        </div>
+        <div className="linkContainer">
+          <a href="https://google.com" className="button">
+            <img 
+              className="facebookLogo"
+              src={fbLogo}
+              alt="fucc the zucc" 
+            />
+            Flush Instagram
+          </a>
+        </div>
       </div>
     )
   }
