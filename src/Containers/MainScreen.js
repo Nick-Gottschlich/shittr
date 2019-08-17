bb687ae4eb1b65236e9c16a5d3f4fcc63847889c
Nick Gottschlich <nicholas.gottschlich@procore.com>
2019-08-17 16:17:56 -0500

    updates

--- src/Containers/MainScreen.js
+++ src/Containers/MainScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 // Local
 import fbLogo from '../assets/fbLogo.png';
+import igLogo from '../assets/igLogo.png';
 
 // CSS
 import './MainScreen.css';
@@ -11,24 +12,27 @@ export default class MainScreen extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="linkContainer">
+        <div className="linkContainer facebook">
           <a 
             href="https://www.facebook.com/help/delete_account" 
             target="_blank" 
-            className="button">
+            className="flex">
             <img 
-              className="facebookLogo"
+              className="siteLogo"
               src={fbLogo}
               alt="fucc the zucc" 
             />
             Flush Facebook
           </a>
         </div>
-        <div className="linkContainer">
-          <a href="https://google.com" className="button">
+        <div className="linkContainer instagram">
+          <a 
+            href="https://www.instagram.com/accounts/remove/request/permanent/" 
+            className="flex"
+            target="_blank">
             <img 
-              className="facebookLogo"
-              src={fbLogo}
+              className="siteLogo"
+              src={igLogo}
               alt="fucc the zucc" 
             />
             Flush Instagram
