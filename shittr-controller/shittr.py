a00cae7a375180cacf749a3052df457f2720b977
@NickGottschlich <@NickGottschlich>
2019-11-02 18:59:31 -0500

    magnetic switch working, can't fully delete accounts

--- shittr-controller/shittr.py
+++ shittr-controller/shittr.py
@@ -19,24 +19,19 @@ def reset():
     browser.get('localhost:3000')
 
 
+def run_flush():
+    play(flush_noise)
+    time.sleep(3)
+
+
 def run_this_shit(window):
     browser.get('http://localhost:3000')
     browser.maximize_window()
     browser.fullscreen_window()
 
-    # GPIO.setmode(GPIO.BCM)
-    # GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    # input_state = GPIO.input(18)
-
     while True:
         input_state = GPIO.input(18)
-        # if input_state == False:
-            # print('Flushed yo shit')
-            # play(flush_noise)
-            # time.sleep(3)
 
-        # fakeButtonPress = input()
-        # if fakeButtonPress == 'go':
         if input_state == False:
             print(browser.current_url)
             url = browser.current_url
@@ -48,14 +43,14 @@ def run_this_shit(window):
                 fb_delete_button = browser.find_element_by_xpath(
                     '/html/body/div[1]/div[3]/div[1]/div/div/div[3]/div/div[1]/a[2]')
                 fb_delete_button.click()
-                time.sleep(2)
-                play(flush_noise)
+                # run_flush()
 
-                #test from here
                 input_state = GPIO.input(18)
 
-                # fakeButtonPress2 = input()
-                # if fakeButtonPress2 == 'go':
+                # this is the breaking point, the if else chain runs all the way down every 0.5 seconds
+                #  so I can't just keep grabbing input
+                # will need to figure out a different way
+
                 if input_state == False:
                     # confirmation button once the user enters password
                     fb_confirm_button = browser.find_element_by_xpath(
@@ -63,15 +58,13 @@ def run_this_shit(window):
                     fb_confirm_button.click()
                     time.sleep(2)
 
-                    # fakeButtonPress3 = input()
-                    # if fakeButtonPress3 == 'go':
                     if input_state == False:
                         # really confirm button once the user presses confirm eat a dick zucc
                         fb_really_confirm_button = browser.find_element_by_xpath(
                             '/html/body/div[7]/div[2]/div/div/form/div[3]/button')
                         fb_really_confirm_button.click()
                         time.sleep(2)
-                        reset(browser)
+                        reset()
 
             if url == 'https://www.instagram.com/accounts/remove/request/permanent/':
                     ig_delete_button = browser.find_element_by_xpath(
@@ -83,7 +76,7 @@ def run_this_shit(window):
                         browser.switch_to.alert.accept()
 
                         time.sleep(2)
-                        reset(browser)
+                        reset()
 
             if url == 'https://twitter.com/settings/deactivate':
                 twitter_delete_button = browser.find_element_by_xpath(
@@ -97,7 +90,7 @@ def run_this_shit(window):
                     twitter_confirm_delete_button.click()
 
                     time.sleep(2)
-                    reset(browser)
+                    reset()
 
             if url == 'https://www.reddit.com/settings':
                 reddit_delete_button = browser.find_element_by_xpath(
@@ -117,11 +110,9 @@ def run_this_shit(window):
                         reddit_really_confirm_delete_button.click()
 
                         time.sleep(2)
-                        reset(browser)
+                        reset()
 
-        # if fakeButtonPress == 'stop':
-        #     print('resetting')
-        #     reset(browser)
+    time.sleep(0.3)
 
 
 if __name__ == '__main__':
