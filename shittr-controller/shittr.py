59f77ab74d19171501557964a8ced0e6c3a93ab4
@NickGottschlich <@NickGottschlich>
2019-11-03 18:01:50 -0600

    by god I think I've done it

--- shittr-controller/shittr.py
+++ shittr-controller/shittr.py
@@ -24,6 +24,115 @@ def run_flush():
     time.sleep(3)
 
 
+def flush_facebook_2():
+    input_state = GPIO.input(18)
+
+    if input_state == False:
+        # really confirm button once the user presses confirm eat a dick zucc
+        fb_really_confirm_button = browser.find_element_by_xpath(
+            '/html/body/div[7]/div[2]/div/div/form/div[3]/button')
+        fb_really_confirm_button.click()
+
+        run_flush()
+        # time.sleep(2)
+
+        reset()
+        return
+    else:
+        time.sleep(0.3)
+        flush_facebook_2()
+
+
+def flush_facebook():
+    input_state = GPIO.input(18)
+
+    if input_state == False:
+        # confirmation button once the user enters password
+        fb_confirm_button = browser.find_element_by_xpath(
+            '/html/body/div[7]/div[2]/div/div/form/div[3]/button')
+        fb_confirm_button.click()
+        
+        run_flush()
+        # time.sleep(2)
+
+        flush_facebook_2()
+        return
+    else:
+        time.sleep(0.3)
+        flush_facebook()
+
+
+def flush_instagram():
+    input_state = GPIO.input(18)
+
+    if input_state == False:
+        browser.switch_to.alert.accept()
+
+        run_flush()
+        # time.sleep(2)
+
+        reset()
+        return
+    else:
+        time.sleep(0.3)
+        flush_instagram()
+
+
+def flush_twitter():
+    input_state = GPIO.input(18)
+
+    if input_state == False:
+        twitter_confirm_delete_button = browser.find_element_by_xpath(
+            '/html/body/div/div/div/div/main/div/div/div/section[2]/div[2]/div[5]/div/div/span/span'
+        )
+        twitter_confirm_delete_button.click()
+
+        run_flush()
+        # time.sleep(2)
+
+        reset()
+        return
+    else:
+        time.sleep(0.3)
+        flush_twitter()
+
+
+def flush_reddit_2():
+    input_state = GPIO.input(18)
+
+    if input_state == False:
+        reddit_really_confirm_delete_button = browser.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div/div[4]/div/div/div/div/div/button[2]')
+        reddit_really_confirm_delete_button.click()
+
+        run_flush()
+        # time.sleep(2)
+
+        reset()
+        return
+    else:
+        time.sleep(0.3)
+        flush_reddit_2()
+
+
+def flush_reddit():
+    input_state = GPIO.input(18)
+
+    if input_state == False:
+        reddit_confirm_delete_button = browser.find_element_by_xpath(
+            '/html/body/div[1]/div/div/div/div[4]/div/div/div/div/div[4]/button[2]')
+        reddit_confirm_delete_button.click()
+
+        run_flush()
+        # time.sleep(2)
+
+        flush_reddit_2()
+        return
+    else:
+        time.sleep(0.3)
+        flush_reddit()
+
+
 def run_this_shit(window):
     browser.get('http://localhost:3000')
     browser.maximize_window()
@@ -37,80 +146,41 @@ def run_this_shit(window):
             url = browser.current_url
 
             if url == 'https://www.facebook.com/help/delete_account':
-                input_state = GPIO.input(18)
-
                 # initial delete button
                 fb_delete_button = browser.find_element_by_xpath(
                     '/html/body/div[1]/div[3]/div[1]/div/div/div[3]/div/div[1]/a[2]')
                 fb_delete_button.click()
                 # run_flush()
+                time.sleep(2)
 
-                input_state = GPIO.input(18)
-
-                # this is the breaking point, the if else chain runs all the way down every 0.5 seconds
-                #  so I can't just keep grabbing input
-                # will need to figure out a different way
-
-                if input_state == False:
-                    # confirmation button once the user enters password
-                    fb_confirm_button = browser.find_element_by_xpath(
-                        '/html/body/div[7]/div[2]/div/div/form/div[3]/button')
-                    fb_confirm_button.click()
-                    time.sleep(2)
-
-                    if input_state == False:
-                        # really confirm button once the user presses confirm eat a dick zucc
-                        fb_really_confirm_button = browser.find_element_by_xpath(
-                            '/html/body/div[7]/div[2]/div/div/form/div[3]/button')
-                        fb_really_confirm_button.click()
-                        time.sleep(2)
-                        reset()
+                flush_facebook()
 
             if url == 'https://www.instagram.com/accounts/remove/request/permanent/':
-                    ig_delete_button = browser.find_element_by_xpath(
+                ig_delete_button = browser.find_element_by_xpath(
                     '/html/body/div/div[1]/div/div[2]/section/form/div[8]/p[4]/input')
-                    ig_delete_button.click()
+                ig_delete_button.click()
+                # run_flush()
+                time.sleep(2)
 
-                    fakeButtonPress2 = input()
-                    if fakeButtonPress2 == 'go':
-                        browser.switch_to.alert.accept()
-
-                        time.sleep(2)
-                        reset()
+                flush_instagram()
 
             if url == 'https://twitter.com/settings/deactivate':
                 twitter_delete_button = browser.find_element_by_xpath(
                     '/html/body/div/div/div/div/main/div/div/div/section[2]/div[2]/div/div[11]/div/div/span')
                 twitter_delete_button.click()
+                # run_flush()
+                time.sleep(2)
 
-                fakeButtonPress2 = input()
-                if fakeButtonPress2 == 'go':
-                    twitter_confirm_delete_button = browser.find_element_by_xpath(
-                        '/html/body/div/div/div/div/main/div/div/div/section[2]/div[2]/div[5]/div/div/span/span')
-                    twitter_confirm_delete_button.click()
-
-                    time.sleep(2)
-                    reset()
+                flush_twitter()
 
             if url == 'https://www.reddit.com/settings':
                 reddit_delete_button = browser.find_element_by_xpath(
                     '/html/body/div[1]/div/div/div/div[2]/div/div/div[2]/div[1]/div[7]/button')
                 reddit_delete_button.click()
+                # run_flush()
+                time.sleep(2)
 
-                fakeButtonPress2 = input()
-                if fakeButtonPress2 == 'go':
-                    reddit_confirm_delete_button = browser.find_element_by_xpath(
-                        '/html/body/div[1]/div/div/div/div[4]/div/div/div/div/div[4]/button[2]')
-                    reddit_confirm_delete_button.click()
-
-                    fakeButtonPress3 = input()
-                    if fakeButtonPress3 == 'go':
-                        reddit_really_confirm_delete_button = browser.find_element_by_xpath(
-                            '/html/body/div[1]/div/div/div/div[4]/div/div/div/div/div/button[2]')
-                        reddit_really_confirm_delete_button.click()
-
-                        time.sleep(2)
-                        reset()
+                flush_reddit()
 
     time.sleep(0.3)
 
